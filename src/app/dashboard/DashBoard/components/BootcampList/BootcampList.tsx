d232c5da02518f0cacde2fd2d67e966544a65489
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-02 20:08:42 -0600

    feat: add style and modifying some structre

--- src/app/dashboard/DashBoard/components/BootcampList/BootcampList.tsx
+++ src/app/dashboard/DashBoard/components/BootcampList/BootcampList.tsx
@@ -1,40 +1,46 @@
 import { Bootcamp } from '@/interfaces';
+import styles from './BootcampList.module.css';
+import Image from 'next/image';
 
 interface BootcampListProps {
-    bootcamps: Bootcamp[];
-    isInscrito: (bootcampId: number) => boolean;
-    onDesinscripcion: (bootcampId: number) => void;
-    onInscripcion: (bootcampId: number) => void;
-  }
-  
-  export const BootcampList: React.FC<BootcampListProps> = ({
-    bootcamps,
-    isInscrito,
-    onDesinscripcion,
-    onInscripcion
-  }) => {
-    return (
-      <div>
-        {bootcamps.map((bootcamp) => (
-          <div key={bootcamp.id}>
-            <h2>{bootcamp.nombre}</h2>
-            <img 
-              src={bootcamp.imagen_url} 
-              alt={bootcamp.nombre}
-              style={{ maxWidth: '300px' }}
-            />
-            <p>{bootcamp.descripcion}</p>
-            {isInscrito(bootcamp.id) ? (
-              <button onClick={() => onDesinscripcion(bootcamp.id)}>
-                Desinscribirse
-              </button>
-            ) : (
-              <button onClick={() => onInscripcion(bootcamp.id)}>
-                Inscribirse
-              </button>
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  };
+  bootcamps: Bootcamp[];
+  isInscrito: (bootcampId: number) => boolean;
+  onDesinscripcion: (bootcampId: number) => void;
+  onInscripcion: (bootcampId: number) => void;
+}
+
+export const BootcampList: React.FC<BootcampListProps> = ({
+  bootcamps,
+  isInscrito,
+  onDesinscripcion,
+  onInscripcion,
+}) => {
+  return (
+    <div className={styles.container}>
+      {bootcamps.map((bootcamp) => (
+        <div key={bootcamp.id} className={styles.card}>
+          <Image
+            src={bootcamp.imagen_url}
+            alt={bootcamp.nombre}
+            className={styles.image}
+            priority
+            layout="responsive" 
+            width={100} 
+            height={100} 
+          />
+          <h2>{bootcamp.nombre}</h2>
+          <p>{bootcamp.descripcion}</p>
+          {isInscrito(bootcamp.id) ? (
+            <button onClick={() => onDesinscripcion(bootcamp.id)} className={styles.button}>
+              Desinscribirse
+            </button>
+          ) : (
+            <button onClick={() => onInscripcion(bootcamp.id)} className={styles.button}>
+              Inscribirse
+            </button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
