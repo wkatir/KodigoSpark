d7fba0829e25bbb427150c52f39627ef34c4a771
PachecoKaty <pachecokaty121@gmail.com>
2024-11-03 21:10:11 -0600

    feat(pages): create notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/app/not-found.module.css';
 
 const notFound = () => {
   return (
-    <div>In process</div>
+    <div className={styles.container}>
+      <div className={styles.message}>
+        <h1>404 - Ups, parece que te perdiste </h1>
+        <Link href='/'>Volver al inicio</Link>
+      </div>
+      <Image
+        src='/404-not-found.svg'
+        alt='Not found page'
+        width={550}
+        height={400}
+        priority
+      />
+    </div>
   );
 };
 
