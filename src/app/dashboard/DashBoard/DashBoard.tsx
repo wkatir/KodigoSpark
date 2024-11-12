686a29a8e80e077c27e3743916b69b087fae3393
PachecoKaty <pachecokaty121@gmail.com>
2024-11-11 21:33:56 -0600

    style(responsive): add responsive design to multiple pages

--- src/app/dashboard/DashBoard/DashBoard.tsx
+++ src/app/dashboard/DashBoard/DashBoard.tsx
@@ -6,7 +6,6 @@ import { Navbar } from '@/shared/components/Navbar/Navbar';
 import styles from '@/app/dashboard/DashBoard/DashBoard.module.css';
 import Footer from '@/shared/components/Footer/Footer';
 import { InscripcionForm } from './components/InscriptionForm/InscriptionForm';
-import LogoutButton from './components/LogoutButton/LogoutButton';
 import { Loading } from '@/shared/components/Loading/Loading';
 
 export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
@@ -22,18 +21,17 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
   } = useBootcamps(supabase);
 
   if (!user) {
-    return <Loading></Loading>; 
+    return <Loading></Loading>;
   }
 
   const selectedBootcampData = bootcamps.find(b => b.id === selectedBootcamp) ?? null;
 
   return (
     <>
+      <Navbar />
       <div className={styles.header}>
-        <Navbar />
         <div className={styles.user}>
           <span>{user.email}</span>
-          <LogoutButton></LogoutButton>
         </div>
       </div>
 
@@ -70,7 +68,7 @@ export default function Dashboard({ supabase }: { supabase: SupabaseClient }) {
         )}
       </div>
 
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,16 +1,36 @@
+import LogoutButton from '@/app/dashboard/DashBoard/components/LogoutButton/LogoutButton';
 import styles from '@/shared/components/Navbar/Navbar.module.css';
 import Link from 'next/link';
+import { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className={styles.navbar}>
-            <div>
-                <h1 className={styles.title}>Kodigo Spark</h1>
-            </div>
-            <div className={styles.content}>
-                <Link href='/'>Cursos</Link>
-                <Link href=''>FAQ</Link>
-            </div>
-        </nav>
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.navHeader}>
+                    <h1 className={styles.title}>Kodigo Spark</h1>
+                    <div className={styles.menu} onClick={toggleMenu}>
+                        <GiHamburgerMenu />
+                    </div>
+                </div>
+                <div className={`${styles.content} ${isMenuOpen ? styles.open : ''}`}>
+                    <Link href='/'>Cursos</Link>
+                    <Link href='/faq'>FAQ</Link>
+                    <div className={styles.mobileLogout}>
+                        <LogoutButton></LogoutButton>
+                    </div>
+                </div>
+                <div className={styles.desktopLogout}>
+                    <LogoutButton />
+                </div>
+            </nav>
+        </>
     );
 };
